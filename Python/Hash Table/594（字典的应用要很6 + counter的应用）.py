73264abad139cb9b00ae6f5645fc597961bae027
吴臻 <zw@s-MacBook-Pro.home>
2019-12-06 18:49:09 -0500

    add 2 question

---
+++ Python/Hash Table/594（字典的应用要很6 + counter的应用）.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        from collections import Counter
+        cnt = Counter(nums)
+        #print(cnt)
+        cnt = sorted(cnt.items())  #此处我们的想法其实就是，让key从小到大进行排序，而后我们只要将相差为1的key的value相加，然后就是长度了，然后取最长的组合
+        #print(cnt)
+        ans = 0
+        for i in range(1,len(cnt)):
+            #print(key,value)
+            #print(i)
+            #print(cnt[i][0])
+            if cnt[i][0] == cnt[i - 1][0] + 1:
+                total = cnt[i][1] + cnt[i - 1][1]
+                #print(total)
+                maxi = max(ans,total)
+                ans = total
+        return maxi
+
+
+
+#上面这种解法在遇到了【1，1，1，1】这种情况就会报错，当出现【1，1，1，1】的时候，结果其实应该是0
+#这里我们要直到一件事情就是，我们如果在比较大小的时候，其实在循环里面的值最好不要返回，因为一旦进入不了循环，那么就没有返回值了
+#gang帮我改的结果如下，这样子就可以解决这个问题了
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        from collections import Counter
+        cnt = Counter(nums)
+        #print(cnt)
+        cnt = sorted(cnt.items())
+        #print(cnt)
+        res = 0
+        for i in range(1, len(cnt)):
+            #print(key,value)
+            #print(i)
+            #print(cnt[i][0])
+            if cnt[i][0] == cnt[i - 1][0] + 1:
+                total = cnt[i][1] + cnt[i - 1][1]
+                #print(total)
+                res = max(res,total)
+        return res
+
---
+++ Python/Hash Table/811 Subdomain Visit Count（区域名的连接+字典+split）.py
@@ -0,0 +1,46 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        dic = {}
+        for i in cpdomains:
+            num,dom = i.split()  #默认分隔符是空格，然后前面空格前，后面空格后
+            num,dom = int(num),dom.split('.')   #此处一定要注意将num转变为数字
+            for j in range(len(dom)):
+                str_ = '.'.join(dom[j:])  #这一部分相当于我将后面domain的部分开始一个个连接
+                #这里我们要注意，一定不能写成str不然到后来我们要把int改为str的时候，python是不知道我们要干什么的
+                #print(str)
+                if str_ in dic:  #来修改字典中的value
+                    dic[str_] = dic[str_] + num  
+                else:
+                    dic[str_] = num
+        #print(dic)
+        l = []
+        for q in dic:  #这抽的就是key，我看到有人写for key,val in dic.items():也是可以的，相当于我们同时抽出key和value
+            l.append(str( dic[q] ) + ' ' + q)  
+        return l
+        #其实最后一部分可以直接写成
+        #return [str( dic[q] ) + ' ' + q for q in dic]
+        #这样子就不需要再一个个添加到list里面而会自动添加
+
+
+#别人写的更简单的写法
+    def subdomainVisits(self, cpdomains):
+        c = collections.Counter()
+        for cd in cpdomains:
+            n, d = cd.split()
+            c[d] += int(n)
+            for i in range(len(d)):
+                if d[i] == '.': 
+                    c[d[i + 1:]] += int(n)
+        return ["%d %s" % (c[k], k) for k in c]
+
+
+#下面这个答案也是有点意思，他的想法就是把最开始用空格来做切割符的全部变成.来做切割符
+#然后再进行join连接
+class Solution:
+    def subdomainVisits(self, cpdomains):
+        counter = collections.Counter()
+        for cpdomain in cpdomains:
+            count, *domains = cpdomain.replace(" ",".").split(".")
+            for i in range(len(domains)):
+                counter[".".join(domains[i:])] += int(count)
+        return [" ".join((str(v), k)) for k, v in counter.items()]
