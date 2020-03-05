a53a2947269116b76f13b57077fef411645e78a8
Zhen Wu <50920640+Jeanzw@users.noreply.github.com>
2020-03-05 13:41:10 -0800

    Update 180. Consecutive Numbers.sql

--- SQL/180. Consecutive Numbers.sql
+++ SQL/180. Consecutive Numbers.sql
@@ -3,7 +3,11 @@ from Logs a   #注意，我们这里用的是inner join而不是left join
 inner join Logs b on b.id = a.id+1 and b.num = a.num
 inner join Logs c on c.id = a.id+2 and c.num = a.num
 
-
+--这里其实不是说不能用left join，而是如果用left join需要考虑为null的事情，而且如果用left join我们就是要选择最右边的数，而不是最左边的
+select distinct c.Num as ConsecutiveNums  from Logs a
+left join Logs b on a.Id - 1 = b.Id and a.Num = b.Num
+left join Logs c on b.Id - 1 = c.Id and b.Num = c.Num
+where c.Num is not null
 
 
 #或者
@@ -11,3 +15,5 @@ select distinct a.Num as ConsecutiveNums from Logs a, Logs b, Logs c
 where a.Id + 1 = b.Id
 and b.Id + 1 = c.Id
 and a.Num = b.Num and b.Num = c.Num
+
+
