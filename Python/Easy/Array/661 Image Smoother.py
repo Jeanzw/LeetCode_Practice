52cdbea4e9737a54dd65628a5d80073d16df9bb7
吴臻 <zw@s-MacBook-Pro.home>
2020-06-07 23:23:49 -0700

    661 python

--- Python/Easy/Array/661 Image Smoother.py
+++ Python/Easy/Array/661 Image Smoother.py
@@ -1,5 +1,10 @@
 #https://www.pythonforbeginners.com/basics/list-comprehensions-in-python
 #首先我们要来看看这个链接学会一下list comprehension，如何可以快速利用好if和for来得到我们想要的内容
+# 这一道题目的思路其实很简单：
+# 我们选中一个格子，扫描其周边8个格子
+# 由于我们是基于各自格子的，所以我们的分母是用1开始的，而分子也是基于这个格子的数字进行的
+
+
 class Solution:
     def imageSmoother(self, M: List[List[int]]) -> List[List[int]]:
         row = len(M)
@@ -10,8 +15,8 @@ class Solution:
         #print(res)   #这样就创建出了和原array结构一样的array了
         for i in range(row):
             for j in range(col):
-                total_cell = 1  #这里我们是准备采用计数的方法来得到分母
-                total_value = M[i][j]  #这里就是至少会计算当前格子的值，就是分母
+                total_cell = 1  #这里我们是准备采用计数的方法来得到分母，由于我们是基于格子的，所以必定有一个
+                total_value = M[i][j]  #这里就是至少会计算当前格子的值，就是分子
                 if i - 1 >= 0 and j - 1 >= 0:  #我们这里看的就是左上角的格子
                     total_cell += 1
                     total_value += M[i - 1][j - 1]
