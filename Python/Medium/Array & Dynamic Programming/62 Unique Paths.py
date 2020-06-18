ffdb7386327a2ab273636e45a4585e4f70a6bcbf
吴臻 <zw@s-MacBook-Pro.home>
2020-06-17 23:42:35 -0700

    revise

--- Python/Medium/Array & Dynamic Programming/62 Unique Paths.py
+++ Python/Medium/Array & Dynamic Programming/62 Unique Paths.py
@@ -39,13 +39,13 @@ class Solution(object):
 
 
 #下面是上面题目的解释，比如说我们一个m = 7 n = 3
-start           1              1
-1               1+1 = 2        2+1 = 3
-1               1+2 = 3        3+3 = 6
-1               1+3 = 4        4+6 = 10
-1               1+4 = 5        5+10 = 15
-1               1+5 = 6        6+15 = 21
-1               1+6 = 7        7+21 = 28 end
+start     ｜      1          ｜    1
+1         ｜      1+1 = 2    ｜    2+1 = 3
+1         ｜      1+2 = 3    ｜    3+3 = 6
+1         ｜      1+3 = 4    ｜    4+6 = 10
+1         ｜      1+4 = 5    ｜    5+10 = 15
+1         ｜      1+5 = 6    ｜    6+15 = 21
+1         ｜      1+6 = 7    ｜    7+21 = 28 end
 
 #Gang的做法，先初始化一个全为1的列表，因为我们都是一步步走的
 class Solution(object):
@@ -56,7 +56,7 @@ class Solution(object):
         :rtype: int
         """
         dp = [[1] * n for i in range(m)]
-        for i in range(1,m):
+        for i in range(1,m):  #我们之所以这里都是从1开始，是因为我们是不算start的时候的，我们永远都会走一步
             for j in range(1,n):
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m-1][n-1] 
