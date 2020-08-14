57cb210aff1d33edc7f87ed284515b232aba5300
吴臻 <zw@s-MacBook-Pro.home>
2020-08-13 23:26:30 -0700

    Update 1212. Team Scores in Football Tournament(union all主客场分别处理).sql

--- SQL/Medium/1212. Team Scores in Football Tournament(union all主客场分别处理).sql
+++ SQL/Medium/1212. Team Scores in Football Tournament(union all主客场分别处理).sql
@@ -22,3 +22,29 @@ group by team) tmp
 
 on t.team_id = tmp.team
 order by num_points desc,team_id
+
+
+
+
+-- 第二次写的：
+select team_id,team_name, ifnull(sum(score),0) as num_points from
+Teams t
+left join
+(select host_team as team,
+sum(case when host_goals  > guest_goals then 3 
+ when host_goals  = guest_goals then 1
+ else 0 end ) as score
+ from Matches 
+ group by 1
+ 
+ union all
+ 
+select guest_team as team,
+sum(case when host_goals  < guest_goals then 3 
+ when host_goals  = guest_goals then 1
+ else 0 end ) as score
+ from Matches 
+ group by 1) a
+ on a.team = t.team_id
+ group by 1,2
+ order by num_points desc, team_id
