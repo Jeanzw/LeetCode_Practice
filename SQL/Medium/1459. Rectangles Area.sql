2d1515f887a9b4de54b6429fb7ca086ad7e511b6
吴臻 <zw@s-MacBook-Pro.home>
2020-09-09 21:04:47 -0700

    Create 1459. Rectangles Area.sql

---
+++ SQL/Medium/1459. Rectangles Area.sql
@@ -0,0 +1,14 @@
+-- 我们这道题不要想的太复杂了
+-- 我们注意到题目中又一个这样的限定：p1 and p2 are the id of two opposite corners of a rectangle and p1 < p2.
+-- 那么其实就说明，这两点就是对角的两个点，那么我们只要保证这两点的横纵坐标都是不一样的
+select 
+    a.id as p1,
+    b.id as p2,
+    abs(a.x_value - b.x_value) * abs(a.y_value - b.y_value) as area
+    from Points a
+    join Points b
+    on a.id < b.id  
+--在这里我们保证了a的id是小于b的id的，这样其实就保证了一个顺序性，因为题目中说了p1 and p2 are the id of two opposite corners of a rectangle and p1 < p2.
+    and a.x_value != b.x_value
+    and a.y_value != b.y_value
+    order by 3 desc, 1,2
