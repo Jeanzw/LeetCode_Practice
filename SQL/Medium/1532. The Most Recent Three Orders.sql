ee8a237919ff6abc1a7d64e854e5794cb52b731b
吴臻 <zw@s-MacBook-Pro.home>
2020-09-13 14:58:29 -0700

    Create 1532. The Most Recent Three Orders.sql

---
+++ SQL/Medium/1532. The Most Recent Three Orders.sql
@@ -0,0 +1,5 @@
+select name as customer_name, tmp.customer_id, order_id, order_date from
+(select customer_id, order_id, order_date, rank() over(partition by customer_id order by order_date desc) as rnk from Orders)tmp
+join Customers c on tmp.customer_id = c.customer_id
+where rnk <= 3
+order by name, customer_id, order_date desc
