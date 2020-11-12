ec47d7de0d4fb73f7e8ec1803f755191d968e302
吴臻 <zw@s-MacBook-Pro.home>
2020-11-11 23:23:56 -0800

    176

--- SQL/Easy/176. Second Highest Salary.sql
+++ SQL/Easy/176. Second Highest Salary.sql
@@ -6,7 +6,7 @@ select Salary as SecondHighestSalary from Employee
 where Salary < (select max(Salary) from Employee)
 order by salary desc
 limit 1
-因为这样如果我们只有一个数，其实返回的是一个[],但是我们想要的是如果只有一个数，那么返回的是null
+--因为这样如果我们只有一个数，其实返回的是一个[],但是我们想要的是如果只有一个数，那么返回的是null
 
 
 /* Write your T-SQL query statement below */
@@ -14,7 +14,7 @@ select Salary as SecondHighestSalary from
 (select Salary, rank() over (order by Salary desc) as rnk from Employee) tmp
 where rnk = 2
 --上面这个情况是属于我们可以保证肯定有2nd的数。但是如果没有2nd呢？那么我们需要输出的是null，但是上面输出的是[]
---如果要达到上面的情况，我是觉得完全可以用case
+--那么下面的这种解法我们就加了一个max，其原理其实就是，如果这个数是不存在的情况下，那么我要去求max那么得出的结果是null，而单纯求这个数其实是不存在的，所以是空
 select max(Salary) as SecondHighestSalary    --这里我们将Salary变成max()
 from (SELECT salary, dense_rank() over(order by salary desc) as myrank
 from Employee
