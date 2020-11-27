0464fc2d990fdd5591ab50f46430cf984d895b92
Jeanzw <zwu93@fordham.edu>
2020-11-26 18:53:27 -0800

    revise

--- SQL/Easy/1068. Product Sales Analysis I.sql
+++ SQL/Easy/1068. Product Sales Analysis I.sql
@@ -1,2 +1,11 @@
 select product_name, year,price from Sales s left join Product p
 on s.product_id = p.product_id
+
+
+select product_name,year,price from Product p
+join Sales s on p.product_id = s.product_id
+
+
+-- 我们其实可以看到上面join的先后顺序是不一样的，前者因为是先Sales后product所以直接用left join就可以
+-- 但是后者如果我们用了left join就会导致Samsung的信息也会被包含进去
+-- 所以我们改用join
