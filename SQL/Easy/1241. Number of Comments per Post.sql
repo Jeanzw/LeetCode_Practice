42f8508f80168eb6da48b64d0fd52302a9127525
Jeanzw <zwu93@fordham.edu>
2020-12-03 18:27:23 -0800

    Dec 3

--- SQL/Easy/1241. Number of Comments per Post.sql
+++ SQL/Easy/1241. Number of Comments per Post.sql
@@ -3,3 +3,19 @@ left join Submissions b
 on a.sub_id = b.parent_id
 where a.parent_id is null
 group by 1
+
+
+-- 第二次做的
+with post as
+(select sub_id as id from Submissions 
+where parent_id is null group by 1)
+, comments as
+(select sub_id,parent_id from Submissions
+where parent_id in (select * from post)
+group by 1,2)
+
+select 
+    id as post_id,
+    count(distinct sub_id) as number_of_comments from post p
+    left join comments c on p.id = c.parent_id
+    group by 1
--- SQL/Medium/1212. Team Scores in Football Tournament(union all主客场分别处理).sql
+++ SQL/Medium/1212. Team Scores in Football Tournament(union all主客场分别处理).sql
@@ -24,6 +24,31 @@ on t.team_id = tmp.team
 order by num_points desc,team_id
 
 
+-- 和上面的原理是一样的，但是用cte会看起来容易理解一点
+with team_score as
+(select team,sum(score) as score from
+(select 
+host_team as team,
+case when host_goals > guest_goals then 3
+when host_goals = guest_goals then 1
+else 0 end as score from Matches
+union all
+select 
+guest_team as team,
+case when host_goals < guest_goals then 3
+when host_goals = guest_goals then 1
+else 0 end as score from Matches)tmp 
+group by 1)
+-- 上面用一个team_score来先将所有组的分数给统计出来
+
+
+select t.*,ifnull(score,0) as num_points from Teams t
+left join team_score ts on t.team_id = ts.team
+order by num_points desc, team_id
+
+
+
+
 
 
 -- 第二次写的：
