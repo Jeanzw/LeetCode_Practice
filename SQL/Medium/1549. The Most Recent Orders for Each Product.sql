5b6e9e91889ac4790315e02c4b6fbd380a728806
Jeanzw <zwu93@fordham.edu>
2020-12-17 19:12:36 -0800

    Dec 16

--- SQL/Medium/1549. The Most Recent Orders for Each Product.sql
+++ SQL/Medium/1549. The Most Recent Orders for Each Product.sql
@@ -14,3 +14,17 @@ from
 top_first
 left join Products p on top_first.product_id = p.product_id
 order by product_name,product_id,order_id
+
+
+-- 我就感觉这道题没必要用cte……太麻烦了
+select product_name,product_id,order_id,order_date from
+(select 
+ product_name,
+ o.product_id,
+ o.order_id,
+ order_date,
+ dense_rank() over (partition by o.product_id order by order_date desc) as rnk
+ from Orders o
+ left join Products p on o.product_id = p.product_id)tmp
+ where rnk = 1
+ order by product_name ,product_id,order_id
--- SQL/Medium/1555. Bank Account Summary.sql
+++ SQL/Medium/1555. Bank Account Summary.sql
@@ -1,16 +1,15 @@
-with trans as
-(select id,sum(num) as total from
-(select paid_by as id, -amount as num from Transactions
+with transaction_money as
+(select id,sum(money) as money from
+(select paid_by as id, - amount as money from Transactions
 union all
-select paid_to as id, amount as num from Transactions)tmp
-group by 1)
+ select paid_to as id, amount as money from Transactions
+)tmp group by 1)
 
-# select * from trans
 select 
-user_id,
-user_name,
-sum(credit + ifnull(total,0)) as credit,
-case when sum(credit + total) <0 then "Yes" else "No" end as credit_limit_breached
-from Users u
-left join trans t on u.user_id = t.id
-group by 1,2
+    user_id,
+    user_name,
+    credit + ifnull(money,0) as credit,
+    case when credit + ifnull(money,0) < 0 then 'Yes' else 'No' end as credit_limit_breached
+    from Users u
+    left join transaction_money t on u.user_id = t.id
+    
