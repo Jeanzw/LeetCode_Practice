1f9580dd94dcedddf00ccb7edce93c3f17417812
Jeanzw <zwu93@fordham.edu>
2021-01-05 19:30:46 -0800

    Jan 5

--- SQL/Easy/181. Employees Earning More Than Their Managers.sql
+++ SQL/Easy/181. Employees Earning More Than Their Managers.sql
@@ -1,3 +1,8 @@
 select e1.Name as Employee from Employee e1
 left join Employee e2 on e1.ManagerId = e2.Id
 where e1.Salary > e2.Salary
+
+
+-- 其实对于这种题目直接用join结果会更明显
+select a.Name as Employee from Employee a
+join Employee b on a.ManagerId = b.Id and a.Salary > b.Salary
