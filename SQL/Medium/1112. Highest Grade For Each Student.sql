598e48fa602ba45349514c4df5b1db82e2f19ad6
Jeanzw <zwu93@fordham.edu>
2021-08-06 19:46:50 -0700

    Aug 6

--- SQL/Medium/1112. Highest Grade For Each Student.sql
+++ SQL/Medium/1112. Highest Grade For Each Student.sql
@@ -14,3 +14,16 @@ select student_id, min(course_id) as course_id, grade
 from pro_t
 where max_grade = grade
 group by student_id;
+
+
+-- 也可以这么做
+select student_id,min(course_id) as course_id,grade
+from
+(select
+student_id,
+course_id,
+grade,
+dense_rank() over (partition by student_id order by grade desc) as rnk
+from Enrollments)tmp
+where rnk = 1
+group by 1,3
