745430c95425edbfec0a9183e9f200ca3d3e316a
Jeanzw <zwu93@fordham.edu>
2021-09-22 17:59:47 -0700

    Sep 22

--- SQL/Easy/613. Shortest Distance in a Line(一维求最短距离，和612进行对比).sql
+++ SQL/Easy/613. Shortest Distance in a Line(一维求最短距离，和612进行对比).sql
@@ -1,2 +1,11 @@
-select min(abs(a.x - b.x)) as shortest from point a,point b
+select min(abs(a.x - b.x)) as shortest 
+from point a,point b
 where a.x != b.x
+
+
+-- 我其实觉得就算是用cross join也不要像上面那样子写
+-- 如果真的用cross join也请写成cross join
+select
+min(abs(a.x - b.x)) as shortest
+from point a
+join point b on a.x != b.x
--- SQL/Medium/614. Second Degree Follower.sql
+++ SQL/Medium/614. Second Degree Follower.sql
@@ -5,7 +5,8 @@ group by b.followee
 /*也可以不用left join，而直接用join求交集
 但是无论如何，有一点容易错的地方就是count里面一定是distinct b.follower
 这个才是每个followee真正的follower个数，不然就会出现重复的情况导致出错*/
-select a.follower,count(distinct b.follower) as num from follow a 
+select a.follower,count(distinct b.follower) as num 
+from follow a 
 join follow b
 on a.follower = b.followee
 group by b.followee
