b458c987486b9c07cb8cc6f9912c665000307d9a
Jeanzw <zwu93@fordham.edu>
2021-10-08 13:46:18 -0700

    Oct 8

--- SQL/Hard/1412. Find the Quiet Students in All Exams.sql
+++ SQL/Hard/1412. Find the Quiet Students in All Exams.sql
@@ -27,3 +27,19 @@ where rnk_desc = 1 or rnk = 1)
 select student_id,student_name from Student
 where student_id not in (select student_id from not_quiet_student)
 and student_id in (select student_id from Exam)
+
+
+
+-- 我们也可以在not in里面讨论rnk的问题
+with not_quiet as
+(select
+student_id,
+dense_rank() over (partition by exam_id order by score) as rnk,
+dense_rank() over (partition by exam_id order by score desc) as rnk_desc
+from Exam)
+
+select
+*
+from Student
+where student_id not in (select student_id from not_quiet where rnk = 1 or rnk_desc = 1)
+and student_id in (select student_id from Exam)
