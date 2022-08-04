df7d45f323fd57680947d990849c715559a8e5b5
Jeanzw <zwu93@fordham.edu>
2022-08-03 21:17:44 -0700

    8/3

---
+++ Python/Python 39/Dictionary/Target Sum.py
@@ -0,0 +1,17 @@
+# 这道题有两种解法：
+# 方法1： 利用双重index去扫描index，但是效率比较低
+def TargetSum(a,b):
+    for i in range(len(a) - 1):
+        for j in range(1, len(a)):
+            if a[i] + a[j] == b:
+                return [i,j]
+    return - 1
+# 方法2：利用dic，key是要寻找的另一半数字，而value就是当前key
+def TargetSum(a,b):
+    dic = {}
+    for i in range(len(a)):
+        if a[i] not in dic.keys():
+            dic[b - a[i]] = i
+        else:
+            return [dic[a[i]],i]
+    return -1
