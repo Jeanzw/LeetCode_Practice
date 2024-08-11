ffc18f23781f896d3b3f5021a3981697d128be82
Jeanzw <jeanwuzh@gmail.com>
2024-08-10 19:57:43 -0700

    Aug 10 update

--- SQL/Medium/2142. The Number of Passengers in Each Bus I.sql
+++ SQL/Medium/2142. The Number of Passengers in Each Bus I.sql
@@ -16,3 +16,38 @@ from bus_info a
 left join Passengers b on b.arrival_time between a.start and a.end
 group by 1
 order by 1
+
+
+-- 我觉得先排序再处理有点慢，直接lag解决
+# Write your MySQL query statement below
+with cte as
+(select
+*,
+ifnull(lag(arrival_time,1) over (order by arrival_time),0) as last_time
+from Buses )
+
+select
+a.bus_id,
+count(distinct b.passenger_id) as passengers_cnt
+from cte a
+left join Passengers b on b.arrival_time <= a.arrival_time and b.arrival_time > a.last_time
+group by 1
+order by 1
+
+
+-- Python
+import pandas as pd
+
+def count_passengers_in_bus(buses: pd.DataFrame, passengers: pd.DataFrame) -> pd.DataFrame:
+    buses = buses.sort_values('arrival_time')
+    buses['last_time'] = buses.arrival_time.shift(1).fillna(0)
+
+    merge = pd.merge(buses,passengers, how = 'cross')
+    merge = merge.query("arrival_time_y <= arrival_time_x and arrival_time_y > last_time")
+
+    summary = merge.groupby(['bus_id'], as_index = False).agg(
+        passengers_cnt = ('passenger_id','nunique')
+    )
+
+    res = pd.merge(buses,summary, on = 'bus_id', how = 'left').fillna(0)
+    return res[['bus_id','passengers_cnt']].sort_values('bus_id')
--- SQL/Medium/2159. Order Two Columns Independently.sql
+++ SQL/Medium/2159. Order Two Columns Independently.sql
@@ -6,3 +6,29 @@ with first_ as
 select first_col,second_col
 from first_ a
 left join second_ b on a.rnk = b.rnk
+
+-- 实在没必要用两个cte
+with frame as
+(select 
+*,
+row_number() over (order by first_col) as first_rnk,
+row_number() over (order by second_col desc) as second_rnk
+from Data
+)
+
+select 
+a.first_col,
+b.second_col
+from frame a
+inner join frame b on a.first_rnk = b.second_rnk
+order by a.first_rnk
+
+
+-- Python
+import pandas as pd
+
+def order_two_columns(data: pd.DataFrame) -> pd.DataFrame:
+    data['first_rank'] =  data['first_col'].rank(method = 'first')
+    data['second_rank'] =  data['second_col'].rank(method = 'first',ascending = False)
+    merge = pd.merge(data,data,left_on = 'first_rank',right_on = 'second_rank').sort_values('first_col_x')
+    return merge[['first_col_x','second_col_y']].sort_values(['first_col_x','second_col_y'], ascending = [1,0]).rename(columns = {'first_col_x':'first_col','second_col_y':'second_col'})
