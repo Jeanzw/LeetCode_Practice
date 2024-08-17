25bbf46a7833de207c153b4b033bd4abb94a94e7
Jeanzw <jeanwuzh@gmail.com>
2024-08-16 23:34:40 -0700

    Aug 16 2024

--- SQL/Hard/2362. Generate the Invoice.sql
+++ SQL/Hard/2362. Generate the Invoice.sql
@@ -13,3 +13,16 @@ a.product_id,a.quantity,price * quantity as price
 from Purchases a
 inner join cte b on a.invoice_id = b.invoice_id
 left join Products c on a.product_id = c.product_id
+
+
+
+-- Python
+import pandas as pd
+
+def generate_the_invoice(products: pd.DataFrame, purchases: pd.DataFrame) -> pd.DataFrame:
+    merge = pd.merge(products,purchases,on = 'product_id')
+    merge['sum_price'] = merge['price'] * merge['quantity']
+    invoice = merge.groupby(['invoice_id'],as_index = False).sum_price.sum().sort_values(['sum_price','invoice_id'], ascending = [0,1]).head(1)
+
+    summary = pd.merge(invoice,merge, on ='invoice_id')
+    return summary[['product_id','quantity','sum_price_y']].rename(columns = {'sum_price_y':'price'})
