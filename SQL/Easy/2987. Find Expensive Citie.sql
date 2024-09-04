1ee7bc6d05003ebe59da10e3521b336f34e9f178
Jeanzw <jeanwuzh@gmail.com>
2024-09-03 19:55:12 -0700

    Sep 3 2024

--- SQL/Easy/2987. Find Expensive Citie.sql
+++ SQL/Easy/2987. Find Expensive Citie.sql
@@ -11,3 +11,22 @@ city
 from city_avg,nation
 where city_avg > nation_avg
 order by 1
+
+-- 其实可以直接一步到位
+with cte as
+(select
+*,
+avg(price) over (partition by city) as avg_city,
+avg(price) over () as avg_nation
+from Listings)
+
+select distinct city from cte where avg_city > avg_nation order by 1
+
+
+-- Python
+import pandas as pd
+
+def find_expensive_cities(listings: pd.DataFrame) -> pd.DataFrame:
+    listings['city_avg'] = listings.groupby(['city']).price.transform(mean)
+    listings['nation_avg'] = listings.price.mean()
+    return listings.query("city_avg>nation_avg")[['city']].drop_duplicates().sort_values('city')
--- SQL/Medium/2988. Manager of the Largest Department.sql
+++ SQL/Medium/2988. Manager of the Largest Department.sql
@@ -12,3 +12,24 @@ from Employees
 where position = 'Manager'
 and dep_id in (select dep_id from cnt where rnk = 1)
 order by 2
+
+
+-- 或者这样做
+with cte1 as
+(select
+*,
+count(*) over (partition by dep_id) as cnt
+from Employees)
+, cte2 as
+(select *, dense_rank() over (order by cnt desc) as rnk from cte1)
+
+select emp_name as manager_name, dep_id from cte2 where rnk = 1 and position = 'Manager' order by 2
+
+
+-- Python
+import pandas as pd
+
+def find_manager(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['cnt'] = employees.groupby(['dep_id']).emp_id.transform('count')
+    employees['rnk'] = employees.cnt.rank(method = 'dense', ascending = False)
+    return employees.query("rnk == 1 and position == 'Manager'")[['emp_name','dep_id']].rename(columns = {'emp_name':'manager_name'}).sort_values('dep_id')
