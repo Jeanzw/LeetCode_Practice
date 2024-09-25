175ab54da0521c16231daf2e48c70081a34b328f
Jeanzw <jeanwuzh@gmail.com>
2024-09-24 19:03:58 -0700

    Sep 24 2024

--- SQL/Easy/196. Delete Duplicate Emails(删除重复值).sql
+++ SQL/Easy/196. Delete Duplicate Emails(删除重复值).sql
@@ -16,7 +16,5 @@ left join Person p2 on p1.Email = p2.Email and p1.Id < p2.Id
 import pandas as pd
 
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
-    min_id = person.groupby('email')['id'].transform('min')
-    removed_person = person[person['id'] != min_id] 
-    person.drop(removed_person.index, inplace=True)
-    return
+    person.sort_values(by='id', inplace=True)
+    person.drop_duplicates(subset='email', keep='first', inplace=True)
--- SQL/Hard/185. Department Top Three Salaries.sql
+++ SQL/Hard/185. Department Top Three Salaries.sql
@@ -14,15 +14,8 @@ where rnk <= 3
 
 -- Python
 import pandas as pd
-​
-def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
-    
-    Employee_Department = employee.merge(department, left_on='departmentId', right_on='id').rename(columns = {'name_y': 'Department'})
 
-    Employee_Department = Employee_Department[['Department', 'departmentId', 'salary']].drop_duplicates()
-    
-    top_salary = Employee_Department.groupby(['Department', 'departmentId']).salary.nlargest(3).reset_index()
-    
-    df = top_salary.merge(employee, on=['departmentId', 'salary'])
-    
-    return df[['Department', 'name', 'salary']].rename(columns = {'name': 'Employee', 'salary': 'Salary'})
+def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    merge = pd.merge(employee,department,left_on = 'departmentId',right_on = 'id')
+    merge['rnk'] = merge.groupby(['departmentId']).salary.rank(method = 'dense', ascending = False)
+    return merge.query("rnk <= 3")[['name_y','name_x','salary']].rename(columns = {'name_y':'Department','name_x':'Employee'})
